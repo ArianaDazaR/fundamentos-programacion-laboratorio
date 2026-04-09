4f6b397b05abd0bfc7bea48ddbf39ae7fd0b7ee1
ArianaDazaR <arianitadaza@gmail.com>
2026-04-09 10:12:10 -0400

    Create PiramideCentradaAsteriscos.cpp

---
+++ Trabajos-Clases/PiramideCentradaAsteriscos.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int altura;
+    int i, k, j;
+
+    cout << "Que altura tendra la piramide?: ";
+    cin >> altura;
+
+    for (i = 1; i <= altura; i++) {
+        for (j = 1; j <= altura - i; j++) {
+            cout << " ";
+        }
+        for (k = 1; k <= (2 * i - 1); k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+return 0;
+}
